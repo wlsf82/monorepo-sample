6c9ff2a018d6759a3471f1e1636f01e65daa0569
Walmyr <wlsf82@gmail.com>
2018-07-31 17:56:21 +0200

    add package and test files for each service

---
+++ service1/test.foo.js
@@ -0,0 +1,6 @@
+const test = require("tape");
+
+test("foo", t => {
+    t.plan(1);
+    t.equal(true, true);
+});
---
+++ service2/test.bar.js
@@ -0,0 +1,6 @@
+const test = require("tape");
+
+test("bar", t => {
+    t.plan(1);
+    t.equal(true, true);
+});
